dc6442d50c12cfafff2980f6b977821381f993d1
Brainfog <zackien7@gmail.com>
2021-10-31 20:18:55 +0700

    first commit

---
+++ src/Components/Todo-list/TodoList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Todolist.css';
+
+function Todolist() {
+
+    return (
+        <div className="todo-list">
+        </div>
+    );
+}
+
+export default Todolist;
